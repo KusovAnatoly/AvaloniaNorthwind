91a2b5ae3700961d6f922afbdec46c9145731aed
Anatoly Kusov <kusov.anatoly@gmail.com>
2024-10-10 22:38:15 +0300

    Minor Changes to Orders View

--- AvaloniaNorthwind/ViewModels/OrdersUserControlViewModel.cs
+++ AvaloniaNorthwind/ViewModels/OrdersUserControlViewModel.cs
@@ -6,6 +6,7 @@ using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -32,6 +33,9 @@ public partial class OrdersUserControlViewModel : ViewModelBase
     [ObservableProperty]
     private DateTimeOffset? _dateRequired;
 
+    [ObservableProperty]
+    private bool _canConnect;
+
     public OrdersUserControlViewModel()
     {
         AsyncLoadOrdersCommand = new AsyncRelayCommand(LoadOrdersAsync);
@@ -50,6 +54,15 @@ public partial class OrdersUserControlViewModel : ViewModelBase
     {
         IsLoading = true;
 
+        using var dbContext = new NorthwindContext();
+
+        CanConnect = await dbContext.Database.CanConnectAsync();
+
+        if (!CanConnect)
+        {
+            return;
+        }
+
         var orders = await GetFilteredOrders();
 
         Orders = new ObservableCollection<Order>(orders);
@@ -59,36 +72,44 @@ public partial class OrdersUserControlViewModel : ViewModelBase
 
     private async Task<IEnumerable<Order>> GetFilteredOrders()
     {
-        using var dbContext = new NorthwindContext();
-
-        var orders = dbContext.Orders
-                                             .Include(x => x.OrderDetails)
-                                             .Include(x => x.Customer)
-                                             .AsQueryable()
-                                             .AsNoTracking();
-
-        if (!string.IsNullOrEmpty(SearchQuery))
+        try
         {
+            using var dbContext = new NorthwindContext();
 
-            orders = orders.Where(order =>
-                order.OrderId.ToString().ToLower().Contains(SearchQuery.ToLower()) ||
-                order.Customer.ContactName.ToLower().Contains(SearchQuery.ToLower()) ||
-                order.Customer.CompanyName.ToLower().Contains(SearchQuery.ToLower()) ||
-                order.Customer.ContactName.ToLower().Contains(SearchQuery.ToLower()) ||
-                order.Customer.Phone.Contains(SearchQuery)
-            );
+            var orders = dbContext.Orders
+                                  .Include(x => x.OrderDetails)
+                                  .Include(x => x.Customer)
+                                  .AsQueryable()
+                                  .AsNoTracking();
+
+            if (!string.IsNullOrEmpty(SearchQuery))
+            {
+                orders = orders.Where(order =>
+                    order.OrderId.ToString().ToLower().Contains(SearchQuery.ToLower()) ||
+                    order.Customer.ContactName.ToLower().Contains(SearchQuery.ToLower()) ||
+                    order.Customer.CompanyName.ToLower().Contains(SearchQuery.ToLower()) ||
+                    order.Customer.ContactName.ToLower().Contains(SearchQuery.ToLower()) ||
+                    order.Customer.Phone.Contains(SearchQuery)
+                );
+            }
+
+            if (Date is not null)
+            {
+                orders = orders.Where(order => order.OrderDate == DateOnly.FromDateTime(Date.Value.Date));
+            }
+
+            if (DateRequired is not null)
+            {
+                orders = orders.Where(order => order.RequiredDate == DateOnly.FromDateTime(DateRequired.Value.Date));
+            }
+
+            return await orders.ToListAsync();
         }
-
-        if (Date is not null)
+        catch (Exception exception)
         {
-            orders = orders.Where(order => order.OrderDate == DateOnly.FromDateTime(Date.Value.Date));
+            Debug.WriteLine(exception.Message);
+            // In case the connection to the database was not established
+            return [];
         }
-
-        if (DateRequired is not null)
-        {
-            orders = orders.Where(order => order.RequiredDate == DateOnly.FromDateTime(DateRequired.Value.Date));
-        }
-
-        return await orders.ToListAsync();
     }
 }
